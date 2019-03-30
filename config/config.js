8dc154b96f9ba10c424ba59a983abe86bd2967ae
Jacob Majors <jacobleemajors@gmail.com>
2019-03-30 13:01:54 -0500

    test push for heroku

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
 require('dotenv');
 module.exports = {
-  development: {
+  "development": {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_KEY,
     database: process.env.MYSQL_DBNAME,
     host: process.env.MYSQL_HOST,
     dialect: "mysql"
   },
-  test: {
+  "test": {
     username: "root",
     password: "root",
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql"
   },
-  production: {
+  "production": {
     // eslint-disable-next-line camelcase
     use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
