4d8ef012c2896cafe74615d016a543c70be670ec
Jacob Majors <jacobleemajors@gmail.com>
2019-03-31 22:03:12 -0500

    heroku test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -34,7 +34,6 @@ module.exports = function(app) {
       res.json(dbGoals);
     });
   });
-<<<<<<< HEAD
     // POST route for saving a new todo
     app.post("/api/userupdate", function(req, res) {
       console.log(req.body);
@@ -49,22 +48,4 @@ module.exports = function(app) {
         res.json(dbGoals);
       });
     });
-=======
-
-
-  // POST route for saving a new todo
-  app.post("/api/userupdate", function(req, res) {
-    console.log(req.body);
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
-    db.PresetGoals.create({
-      text: req.body.text,
-      complete: req.body.complete
-    }).then(function(dbGoals) {
-      // We have access to the new todo as an argument inside of the callback function
-      res.json(dbGoals);
-    });
-  });
->>>>>>> 3acdc4d7f361947f11fb617e3e2047731bfde101
 };
